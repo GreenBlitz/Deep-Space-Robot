d1b9496389409d58127ed3faff7ce6192dc1c950
GutSoftwareLaptop <greenblitz.programmers@gmail.com>
2019-02-27 10:00:34 +0200

    removed redundent chassis prints

--- src/main/java/edu/greenblitz/robotname/subsystems/Chassis.java
+++ src/main/java/edu/greenblitz/robotname/subsystems/Chassis.java
@@ -156,7 +156,6 @@ public class Chassis extends Subsystem {
     private void setLeftRightMotorOutput(double l, double r) {
         m_leftLeader.set(l);
         m_rightLeader.set(r);
-        System.out.println(l + " " + r);
     }
 
     public void setRampingRate(double timeToTopSpeed){
