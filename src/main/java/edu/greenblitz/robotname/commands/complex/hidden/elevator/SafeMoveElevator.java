2102e873ec938317e264551b4b4aaf878482467b
GutSoftwareLaptop <greenblitz.programmers@gmail.com>
2019-03-04 00:50:31 +0200

    MoveElevator is now a dynamic commands which manages the tolerance

--- src/main/java/edu/greenblitz/robotname/commands/complex/hidden/elevator/SafeMoveElevator.java
+++ src/main/java/edu/greenblitz/robotname/commands/complex/hidden/elevator/SafeMoveElevator.java
@@ -1,7 +1,6 @@
 package edu.greenblitz.robotname.commands.complex.hidden.elevator;
 
 import edu.greenblitz.robotname.commands.simple.elevator.ImmediateBrakeElevator;
-import edu.greenblitz.robotname.commands.simple.elevator.MoveElevator;
 import edu.greenblitz.robotname.commands.simple.kicker.Unkick;
 import edu.greenblitz.robotname.commands.simple.poker.RetractPoker;
 import edu.greenblitz.robotname.commands.simple.roller.ExtendRoller;
@@ -13,8 +12,12 @@ import edu.greenblitz.robotname.subsystems.Roller;
 import edu.greenblitz.utils.command.DynamicRequire;
 import edu.greenblitz.utils.command.chain.CommandChain;
 import edu.greenblitz.utils.sm.ElevatorState;
+import edu.wpi.first.wpilibj.GenericHID;
 
 public class SafeMoveElevator extends CommandChain {
+
+    private static final double LOWER_TOLERANCE = 0.01;
+    private static final double HIGHER_TOLERANCE = 1;
 
     private double height;
 
@@ -42,18 +45,11 @@ public class SafeMoveElevator extends CommandChain {
             }
             addSequential(new ExtendRoller());
 
-            if (Elevator.getInstance().getHeight() > height) { // Going down
-                addSequential(new MoveElevator(height, .01, 1));
-            } else { // Going up
-                addSequential(new MoveElevator(height, 1, .01));
-            }
+            addSequential(new MoveElevator(height, LOWER_TOLERANCE, HIGHER_TOLERANCE));
+
             addSequential(new RetractRoller());
         } else {
-            if (Elevator.getInstance().getHeight() > height) { // Going down
-                addSequential(new MoveElevator(height, .01, 1));
-            } else { // Going up
-                addSequential(new MoveElevator(height, 1, .01));
-            }
+            addSequential(new MoveElevator(height, LOWER_TOLERANCE, HIGHER_TOLERANCE));
         }
     }
 
