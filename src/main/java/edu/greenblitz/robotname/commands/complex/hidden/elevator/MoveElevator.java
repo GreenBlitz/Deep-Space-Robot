7005a148a74aa3de0e977e89f45856ad3f075461
GutSoftwareLaptop <greenblitz.programmers@gmail.com>
2019-03-04 00:50:31 +0200

    MoveElevator is now a dynamic commands which manages the tolerance

---
+++ src/main/java/edu/greenblitz/robotname/commands/complex/hidden/elevator/MoveElevator.java
@@ -0,0 +1,34 @@
+package edu.greenblitz.robotname.commands.complex.hidden.elevator;
+
+import edu.greenblitz.robotname.subsystems.Elevator;
+import edu.greenblitz.utils.command.GBCommand;
+import edu.greenblitz.utils.command.dynamic.DynamicCommand;
+
+public class MoveElevator extends DynamicCommand {
+    private double m_height, m_lowerTol, m_higherTol;
+
+    public MoveElevator(double height, double lowerTol, double higherTol) {
+        m_height = height;
+        m_lowerTol = lowerTol;
+        m_higherTol = higherTol;
+        setInterruptible(false);
+        setName("Move elevator to " + height);
+    }
+
+    public MoveElevator(Elevator.Level level) {
+        this(level.heightByCurrentState(), 0.05, 0.05);
+    }
+
+    public MoveElevator(Elevator.Level level, double lower, double higher){
+        this(level.heightByCurrentState(), lower, higher);
+    }
+
+    @Override
+    protected GBCommand pick() {
+        if (Elevator.getInstance().getHeight() > m_height) { // Going down
+            return new MoveElevator(m_height, m_lowerTol, m_higherTol);
+        } else { // Going up
+            return new MoveElevator(m_height, m_higherTol, m_lowerTol);
+        }
+    }
+}
