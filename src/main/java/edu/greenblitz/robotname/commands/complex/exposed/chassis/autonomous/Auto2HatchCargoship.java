256e44a28733937773e6693156ce8638f24009ee
GutSoftwareLaptop <greenblitz.programmers@gmail.com>
2019-03-10 07:22:16 +0200

    reorderd poker operations in auto

--- src/main/java/edu/greenblitz/robotname/commands/complex/exposed/chassis/autonomous/Auto2HatchCargoship.java
+++ src/main/java/edu/greenblitz/robotname/commands/complex/exposed/chassis/autonomous/Auto2HatchCargoship.java
@@ -5,7 +5,6 @@ import edu.greenblitz.robotname.commands.simple.chassis.DriveStraightByDistance;
 import edu.greenblitz.robotname.commands.simple.chassis.DropFromHeight;
 import edu.greenblitz.robotname.commands.simple.chassis.motion.APPCCommand;
 import edu.greenblitz.robotname.commands.simple.chassis.motion.ResetLocalizer;
-import edu.greenblitz.robotname.commands.simple.chassis.neutral.ToCoast;
 import edu.greenblitz.robotname.commands.simple.chassis.vision.DriveToDistanceFromVisionTarget;
 import edu.greenblitz.robotname.commands.simple.poker.ExtendPoker;
 import edu.greenblitz.robotname.commands.simple.poker.ReleaseHatch;
@@ -29,11 +28,12 @@ public class Auto2HatchCargoship extends CommandChain {
         addParallel(new ResetLocalizer(-3.151, 1.6, Math.PI));
         addSequential(new DropFromHeight());
 
-        addSequential(new APPCCommand(Paths.get("Cargoship1"),
-                0.6, 0.2, true, 0.2, 0.3, .7, .2)
+        addParallel(new RetractAndHold(POKER_TIMEOUT),
+                new APPCCommand(Paths.get("Cargoship1"),
+                        0.6, 0.2, true, 0.2, 0.5, .7, .2)
         );
 
-        addParallel(new RetractAndHold(POKER_TIMEOUT), new DriveToDistanceFromVisionTarget(PLACE_ALIGN_DISTANCE));
+        addSequential(new DriveToDistanceFromVisionTarget(PLACE_ALIGN_DISTANCE));
         addParallel(new ExtendPoker(POKER_TIMEOUT), new DriveStraightByDistance(PLACE_ALIGN_DISTANCE - PLACE_EXTEND_DISTANCE, 1000));
         addParallel(new ReleaseHatch(POKER_TIMEOUT), new DriveStraightByDistance(PLACE_EXTEND_DISTANCE - PLACE_ALIGN_DISTANCE, 1000));
 
@@ -51,8 +51,8 @@ public class Auto2HatchCargoship extends CommandChain {
         addParallel(new RetractPoker(POKER_TIMEOUT), new DriveStraightByDistance(COLLECT_EXTEND_DISTANCE - COLLECT_ALIGN_DISTANCE, 1000));
 
         addSequential(new APPCCommand(Paths.get("Cargoship4"),
-                .6, .2, true, .1, 0.5, 0.4, .2
-        ));
+                .6, .2, true, .1, 0.5, 0.4, .2)
+        );
 
         addSequential(new VisionPlaceHatchPanel());
     }
