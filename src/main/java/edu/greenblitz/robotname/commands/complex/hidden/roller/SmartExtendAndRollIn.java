14c539b907a6d42092c5c648beaf5e73b86ad913
GutSoftwareLaptop <greenblitz.programmers@gmail.com>
2019-03-18 17:19:18 +0200

    vision and roller safty testd

---
+++ src/main/java/edu/greenblitz/robotname/commands/complex/hidden/roller/SmartExtendAndRollIn.java
@@ -0,0 +1,30 @@
+package edu.greenblitz.robotname.commands.complex.hidden.roller;
+
+import edu.greenblitz.robotname.commands.simple.roller.RollerBaseCommand;
+import edu.greenblitz.robotname.subsystems.Elevator;
+import edu.greenblitz.utils.command.dynamic.DynamicCommand;
+import edu.greenblitz.utils.sm.RollerState;
+
+public class SmartExtendAndRollIn extends RollerBaseCommand {
+
+    public SmartExtendAndRollIn(long ms) {
+        super(ms);
+    }
+
+    public SmartExtendAndRollIn() {
+    }
+
+    @Override
+    protected RollerState getNextState() {
+        return RollerState.EXTENDED;
+    }
+
+    @Override
+    protected void atInit() {
+        if (Elevator.getInstance().isFloorLevel()) {
+            system.extend();
+            system.rollIn();
+        }
+    }
+
+}
