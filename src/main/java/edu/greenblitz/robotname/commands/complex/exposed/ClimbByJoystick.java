a85d373d411b79b0c07d9abf85d509a335e33e21
GutSoftwareLaptop <greenblitz.programmers@gmail.com>
2019-03-30 19:42:10 +0300

    numbers in vision fixed

--- src/main/java/edu/greenblitz/robotname/commands/complex/exposed/ClimbByJoystick.java
+++ src/main/java/edu/greenblitz/robotname/commands/complex/exposed/ClimbByJoystick.java
@@ -3,6 +3,7 @@ package edu.greenblitz.robotname.commands.complex.exposed;
 import edu.greenblitz.robotname.commands.complex.hidden.climber.StartSideClimberControl;
 import edu.greenblitz.robotname.commands.simple.climber.ClimberBigControlByJoystick;
 import edu.greenblitz.robotname.commands.simple.climber.ClimberProportionalExtendByJoystick;
+import edu.greenblitz.robotname.commands.simple.roller.RollerDoNothing;
 import edu.greenblitz.robotname.commands.simple.shifter.ToPower;
 import edu.greenblitz.utils.command.CommandChain;
 import edu.greenblitz.utils.hid.SmartJoystick;
@@ -12,6 +13,7 @@ public class ClimbByJoystick extends CommandChain {
     private static final double SAFE_POWER = 1.0;
 
     public ClimbByJoystick(SmartJoystick bigJoystick, SmartJoystick extenderJoystick, SmartJoystick driveJoystick) {
+        addParallel(new RollerDoNothing());
         addParallel(new ClimberBigControlByJoystick(SAFE_POWER, bigJoystick));
         addParallel(new ClimberProportionalExtendByJoystick(extenderJoystick));
         addParallel(new StartSideClimberControl(driveJoystick));
