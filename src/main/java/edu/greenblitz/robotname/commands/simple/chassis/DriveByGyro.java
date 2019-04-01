f399dd51d4c047903f65a79baf77787a8cb66ed0
orrkarl <karlor041@gmail.com>
2019-04-01 13:03:40 +0300

    removed redundent code

--- src/main/java/edu/greenblitz/robotname/commands/simple/chassis/DriveByGyro.java
+++ src/main/java/edu/greenblitz/robotname/commands/simple/chassis/DriveByGyro.java
@@ -1,91 +1,3 @@
-//package edu.greenblitz.robotname.commands.simple.chassis;
-//
-//import edu.greenblitz.robotname.subsystems.Chassis;
-//import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-//import org.greenblitz.motion.pid.MultivariablePIDController;
-//import org.greenblitz.motion.pid.PIDObject;
-//import org.greenblitz.motion.tolerance.AbsoluteTolerance;
-//import org.greenblitz.motion.tolerance.ITolerance;
-//
-//public class DriveByGyro extends ChassisBaseCommand {
-//
-//    private static final int DRIVE_IDX = 0;
-//    private static final int TURN_IDX = 1;
-//
-//    private static final PIDObject DRIVE = new PIDObject(0.25, 0, 0);
-//    private static final PIDObject TURN = new PIDObject(0.01, 0, 0);
-//
-//    private static final ITolerance DRIVE_TOL = new AbsoluteTolerance(0.1);
-//    private static final ITolerance TURN_TOL = new AbsoluteTolerance(3);
-//
-//    private static final double POWER_LIMIT = 0.3;
-//
-//    private MultivariablePIDController m_controller;
-//
-//    private static final long TIME_ON_TARGET = 1000;
-//    private long m_onTarget = -1;
-//
-//    private double m_distance;
-//
-//    public DriveByGyro(double distance, long ms) {
-//        super(ms);
-//        m_distance = distance;
-//
-//        m_controller = new MultivariablePIDController(2);
-//        m_controller.setPIDObject(DRIVE_IDX, DRIVE, DRIVE_TOL);
-//        m_controller.setPIDObject(TURN_IDX, TURN, TURN_TOL);
-//    }
-//
-//    public DriveByGyro(double distance) {
-//        m_distance = distance;
-//
-//        m_controller = new MultivariablePIDController(2);
-//        m_controller.setPIDObject(DRIVE_IDX, DRIVE, DRIVE_TOL);
-//        m_controller.setPIDObject(TURN_IDX, TURN, TURN_TOL);
-//    }
-//
-//    @Override
-//    protected void initialize() {
-//        var inputDrive = Chassis.getInstance().getDistance();
-//        var inputTurn = Chassis.getInstance().getAngle();
-//
-//        m_onTarget = -1;
-//
-//        m_controller.configurePID(DRIVE_IDX, inputDrive, inputDrive + m_distance, -POWER_LIMIT, POWER_LIMIT, 0);
-//        m_controller.configurePID(TURN_IDX, inputTurn, inputTurn, -POWER_LIMIT/2, POWER_LIMIT/2, 0);
-//    }
-//
-//    @Override
-//    protected void execute() {
-//        var inputDrive = Chassis.getInstance().getDistance();
-//        var inputTurn = Chassis.getInstance().getAngle();
-//        var pidResult = m_controller.calculate(inputDrive, inputTurn);
-//
-//        Chassis.getInstance().arcadeDrive(pidResult[DRIVE_IDX], -pidResult[TURN_IDX]);
-//
-//        if (m_controller.isFinished(new double[]{inputDrive, inputTurn})) {
-//            if (m_onTarget == -1)
-//                m_onTarget = System.currentTimeMillis();
-//        } else {
-//            m_onTarget = -1;
-//        }
-//    }
-//
-//    @Override
-//    protected boolean isFinished() {
-//        var inputDrive = Chassis.getInstance().getDistance();
-//        var inputTurn = Chassis.getInstance().getAngle();
-//        boolean ret1 = m_controller.isFinished(new double[]{inputDrive, inputTurn});
-//        boolean ret2 = System.currentTimeMillis() - m_onTarget > TIME_ON_TARGET;
-//        return ret1 && ret2;
-//    }
-//
-//    @Override
-//    protected void atEnd() {
-//        Chassis.getInstance().stop();
-//    }
-//}
-
 package edu.greenblitz.robotname.commands.simple.chassis;
 
 import edu.greenblitz.robotname.data.GearDependentDouble;
@@ -124,7 +36,7 @@ public class DriveByGyro extends ChassisBaseCommand implements PIDSource, PIDOut
     private static String generateName(double distance) {
         return DriveByGyro.class.getSimpleName() + " for {" + distance + "}";
     }
-
+    
     public DriveByGyro(double distance, long ms, boolean stopAtEnd, boolean useVisionAngle) {
         super(generateName(distance), ms);
 
