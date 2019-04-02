28cc376fda362818c947a11e41ed917127e46c5b
GutSoftwareLaptop <greenblitz.programmers@gmail.com>
2019-04-02 06:15:07 +0300

    modified paths; auto rocket is failing

--- src/main/java/edu/greenblitz/robotname/commands/complex/chassis/autonomous/AutoFallAndThreeHalfs.java
+++ src/main/java/edu/greenblitz/robotname/commands/complex/chassis/autonomous/AutoFallAndThreeHalfs.java
@@ -16,16 +16,21 @@ import org.greenblitz.motion.base.Position;
 public class AutoFallAndThreeHalfs extends CommandChain {
     private long tStart;
 
-    public AutoFallAndThreeHalfs(boolean left) {
-        addSequential(new FallWithNavx());
+    public AutoFallAndThreeHalfs(boolean left, boolean fall) {
+        if (fall)
+            addSequential(new FallWithNavx());
 
-        addParallel(new ChangeTargetFocus(VisionMaster.Focus.RIGHT),
+        if (left)
+            addParallel(new ChangeTargetFocus(VisionMaster.Focus.RIGHT),
                 new ResetLocalizer(new Position(-3, 1.7, 0)));
+        else
+            addParallel(new ChangeTargetFocus(VisionMaster.Focus.LEFT),
+                    new ResetLocalizer(new Position(-8.2 + 3, 1.7, 0)));
 
         addSequential(new ToSpeed());
 
         addSequential(new APPCCommand(
-                Paths.get("FallAndPlace", left),
+                Paths.get("L_FallAndPlace", left),
                 null,
                 0.8, 0.2, false, 0.3,
                 1, .45, .1, 1.5));
@@ -35,15 +40,19 @@ public class AutoFallAndThreeHalfs extends CommandChain {
         addSequential(new ToSpeed());
         addParallel(new RetractPoker());
 
-        addSequential(new APPCCommand(Paths.get("Cargoship2", left),
+        addSequential(new APPCCommand(Paths.get("L_Cargoship2", left),
                 null, 0.6, 0.6, true,
                 0, 0.75, 0.5, 0.4));
         addSequential(
-                new APPCCommand(Paths.get("Cargoship3", left), null, 2,
+                new APPCCommand(Paths.get("L_Cargoship3", left), null, 2,
                         0.4, false,
                         0.4, 2.7, 1, .1, 2)
         );
         addSequential(new VisionCollectHatchPanel());
+    }
+
+    public AutoFallAndThreeHalfs(boolean left){
+        this(left, true);
     }
 
     @Override
--- src/main/java/edu/greenblitz/robotname/data/Paths.java
+++ src/main/java/edu/greenblitz/robotname/data/Paths.java
@@ -12,34 +12,22 @@ import org.greenblitz.motion.pathing.Path;
 import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Paths {
     private static Logger logger = LogManager.getLogger(Paths.class);
 
-    private static HashMap<String, Path<Position>> paths = new HashMap<>();
-
-    public static final String LEFT_PATH_PREFIX = "L_";
-    public static final String RIGHT_PATH_PREFIX = "R_";
+    private static final double MIDDLE = -4.1;
 
     private static String mkNativePath(String pathname) {
         return "/home/lvuser/deploy/output/" + pathname + ".pf1.csv";
     }
 
-    private static String left(String pathname) {
-        return LEFT_PATH_PREFIX + pathname;
-    }
-
-    private static String right(String pathname) {
-        return RIGHT_PATH_PREFIX + pathname;
-    }
-
+    @Deprecated
     public static Path<Position> getRaw(String pathname) {
-        if (!paths.containsKey(pathname))
-            paths.put(pathname, nativeGetPath(mkNativePath(pathname)));
-        return paths.get(pathname);
+        return nativeGetPath(mkNativePath(pathname));
     }
 
     public static Path<Position> get(String pathname, boolean left) {
@@ -47,29 +35,22 @@ public class Paths {
     }
 
     public static Path<Position> getLeft(String pathname) {
-        return getRaw(left(pathname));
+        return getRaw(pathname);
     }
 
     public static Path<Position> getRight(String pathname) {
-        return getRaw(right(pathname));
-    }
+        Path<Position> pth = getLeft(pathname);
+        List<Position> ret = new ArrayList<>();
 
-    public static void init(String... pathname) {
-        Arrays.stream(pathname).forEach(Paths::init);
-    }
+        for (Position p : pth) {
+            ret.add(new Position(2 * MIDDLE - p.getX(), p.getY(), p.getAngle()));
+        }
 
-    public static void init(String pathame) {
-        File left = new File(mkNativePath(left(pathame)));
-        File right = new File(mkNativePath(right(pathame)));
-        File raw = new File(mkNativePath(pathame));
-
-        if (left.isFile())  getLeft(pathame);
-        if (right.isFile()) getRight(pathame);
-        if (raw.isFile())   getRaw(pathame);
+        return new Path<>(ret);
     }
 
     private static Path<Position> nativeGetPath(String filename) {
-        try(CSVParser read = CSVFormat.DEFAULT.parse(new FileReader(new File(filename)))) {
+        try (CSVParser read = CSVFormat.DEFAULT.parse(new FileReader(new File(filename)))) {
             ArrayList<Position> path = new ArrayList<>();
             List<CSVRecord> records = read.getRecords();
             for (int i = 1; i < records.size(); i++) {
