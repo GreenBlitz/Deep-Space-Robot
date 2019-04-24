6a37fe7dd7a12c9e7d242ddb486ca76a65487b1b
GutSoftwareLaptop <greenblitz.programmers@gmail.com>
2019-04-24 21:57:23 +0300

    hatches no longer drive slowly

--- src/main/java/edu/greenblitz/knockdown/commands/complex/chassis/vision/VisionCollectHatchPanel.java
+++ src/main/java/edu/greenblitz/knockdown/commands/complex/chassis/vision/VisionCollectHatchPanel.java
@@ -56,10 +56,10 @@ public class VisionCollectHatchPanel extends CommandChain {
     private class Forward extends CommandChain {
         private Forward() {
             addSequential(new ExtendPoker(50)); // Needed in different commands for small delay
-            addSequential(new DriveByGyro((ALIGN_DISTANCE - EXTEND_DISTANCE) / 2, 850,
+            addSequential(new DriveByGyro((ALIGN_DISTANCE - EXTEND_DISTANCE) / 2,850,
                     new GearDependentDouble(0.4, 0.4), false));
-            addSequential(new DriveByGyro((ALIGN_DISTANCE - EXTEND_DISTANCE) / 2, 700,
-                    new GearDependentDouble(0.15, 0.15)));
+//            addSequential(new DriveByGyro((ALIGN_DISTANCE - EXTEND_DISTANCE) / 2, 700,
+//                    new GearDependentDouble(0.15, 0.15)));
         }
     }
 
